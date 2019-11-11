103352352b82584c062d9db5168a47ea5bc77631
Engin Polat <enpolat@microsoft.com>
2019-11-11 13:54:11 -0800

    sample usage of idletimeout component

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { IdleTimeOut } from './idle_timeout';
+
+const $idle_time = document.getElementById('idle_time');
+
+console.log(IdleTimeOut.Current);
+
+IdleTimeOut.Current.Reset();
+
+IdleTimeOut.Current.OnTick.subscribe(seconds => {
+  console.log(IdleTimeOut.Current);
+
+  if (seconds <= 4) {
+    $idle_time.innerText = 'everthing is fine! 😀';
+  } else if (seconds > 20) {
+    document.location.href = '/logout';
+  } else {
+    $idle_time.innerText = `page has not been used for ${seconds} seconds`;
+  }
+});
